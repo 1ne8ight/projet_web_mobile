a644f837aeb25e9963ed4cd4c19f29aa4d84c09d
kouadio <kouadioangesophie183@gmail.com>
2025-08-30 10:10:52 +0000

    modèle Recherche

--- optiprice_app/models.py
+++ optiprice_app/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class User (models.Model):
     nom = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+class Recherche(models.Model):
+    mot_cle = models.CharField(max_length=255)
+    categorie = models.CharField(max_length=255)
+    nbre_produits = models.PositiveIntegerField(default=10)
+    date_recherche = models.DateTimeField(auto_now_add=True)
+
+    def _str_(self):
+        return f"{self.mot_cle} ({self.nbre_produits})"
